06ae5f49e23047f2f19274606bd8dd424d9af46d
Jade Kenneth Darunday <jadekennethdarunday@gmail.com>
2025-01-26 19:34:06 +0800

    Remove commented-out color palette and semantic tokens from theme configuration

--- libs/portal/theme/src/theme/index.ts
+++ libs/portal/theme/src/theme/index.ts
@@ -5,35 +5,11 @@ import { tokens } from './tokens';
 
 export * from './recipes';
 export * from './tokens';
-// const palette = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
-
-// const colorObj = Object.entries(
-//   colors.colors as Record<string, Record<number, { value: string }>>
-// )
-//   .map(([key, value]) => {
-//     return ['fg']
-//       .map((variant) => {
-//         return palette.map((d, idx) => {
-//           return {
-//             [`"${[key]}.${[variant]}.${[palette[idx]]}"`]: {
-//               [variant]: { value: value?.[palette[idx]]?.value },
-//             },
-//           };
-//         });
-//       })
-//       .flatMap((color) => color);
-//   })
-//   .flatMap((color) => color);
-
-// const result = Object.assign({}, ...colorObj.map((obj) => obj));
 
 export const customConfig = defineConfig({
   theme: {
     recipes,
     tokens,
-    // semanticTokens: {
-    //   colors: result,
-    // },
   },
   conditions: {
     off: '&:is([data-state=off])',

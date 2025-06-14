c91deab2ee9f01a119155a825b46991542aefb27
jade-kenneth <jadekennethdarunday@gmail.com>
2025-06-15 05:02:10 +0800

    update to static port

--- apps/ecommerce-server/src/main.ts
+++ apps/ecommerce-server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env.PORT || 3002;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(8080, '0.0.0.0');
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
 
